8284d4c5d6a0103f0d65df945ffa9455d46a5dc2
Dafah fabian syah <121594681+dafahfabiansyah@users.noreply.github.com>
2023-01-08 14:03:21 +0700

    Update variabel.js

--- Javascript/variabel.js
+++ Javascript/variabel.js
@@ -6,8 +6,8 @@ var fullName;
 firstName = "dafah"
 middleName = "fabian"
 lastName = "syah"
-fullName = firstName + middleName + fullName;
-console.log(firstName)
-console.log(middleName)
-console.log(lastName)
-console.log(fullName)
+fullName = firstName + middleName + lastName;
+console.log(firstName) //print variabel firstname
+console.log(middleName) //print variabel middlename
+console.log(lastName) //print variabel lastname
+console.log(fullName) //print variabel fullname yang merupakan gabungan dari firstname, middlename, dan lastname
