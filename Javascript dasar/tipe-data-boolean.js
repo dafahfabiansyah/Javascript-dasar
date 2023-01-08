47b5a6e9eb21ad4c84f8dfeebc29ce695a6b506b
dafahfabiansyah <davarobiansyah@gmail.com>
2023-01-08 14:00:44 +0700

    ini adalah update

---
+++ Javascript dasar/tipe-data-boolean.js
@@ -0,0 +1,6 @@
+benar = true;
+console.log(benar)
+salah = false;
+console.log(salah)
+BenarSalah = benar + salah;
+console.log(BenarSalah)
---
+++ Javascript dasar/variabel.js
@@ -0,0 +1,13 @@
+var firstName;
+var middleName;
+var lastName;
+var fullName;
+
+firstName = "dafah"
+middleName = "fabian"
+lastName = "syah"
+fullName = firstName + middleName + lastName;
+console.log(firstName) //print variabel firstname
+console.log(middleName) //print variabel middlename
+console.log(lastName) //print variabel lastname
+console.log(fullName) //print variabel fullname yang merupakan gabungan dari firstname, middlename, dan lastname
