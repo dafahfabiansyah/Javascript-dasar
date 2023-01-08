63bb9853dad30f0178297d14061c7f3db8bf950a
dafahfabiansyah <davarobiansyah@gmail.com>
2023-01-08 14:05:45 +0700

    ini update lagi

--- Javascript dasar/variabel.js
+++ Javascript dasar/variabel.js
@@ -3,9 +3,9 @@ var middleName;
 var lastName;
 var fullName;
 
-firstName = "dafah"
-middleName = "fabian"
-lastName = "syah"
+firstName = "dafah";
+middleName = "fabian";
+lastName = "syah";
 fullName = firstName + middleName + lastName;
 console.log(firstName) //print variabel firstname
 console.log(middleName) //print variabel middlename
